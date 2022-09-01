58c7d526b50fb0af9a747dab700a6771dd347cec
Andrii <104207684+AndriiPinchuk@users.noreply.github.com>
2022-09-01 17:20:59 +0300

    update 4

--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,49 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {myFunction()};
 var scrolled;
 
+
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows: true,
+		dots:true,
+		adaptiveHeight: true,
+		slidesToShow:1,
+		slidesToScroll:1,
+		speed: 2000,
+		easing:'ease',
+		infinite: true,
+		initialSlide: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotHover: true,
+		/*PC*/
+		draggable: true,
+		/*mobile touch*/
+		swipe:true,
+		touchThreshold:10,
+		touchMove:true, //can move
+
+		waitForAnimate:false,
+		centerMode: true,
+		variableWidth: false,
+		/*
+		rows: 1,
+		slidesPerRow:1,
+		*/
+		/*vertical*/
+		/*
+		vertical: false,
+		verticalSwiping:false,
+		*/
+
+
+
+
+	});
+});
+
 function myFunction(){
 var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
 var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -12,7 +55,7 @@ var currentScrollPos = window.pageYOffset;
 if(prevScrollpos>currentScrollPos){document.getElementById("navbar").style.top = "0";}else{document.getElementById("navbar").style.top="-50px";}
 prevScrollpos = currentScrollPos;
 if (scrolled>20){document.getElementById("go-to-top-button").style.display="block";}else{document.getElementById("go-to-top-button").style.display="none";}
-if (scrolled>52){
+if (scrolled>60){
 	document.body.style.backgroundImage="url('img/codeImg2.jpg')";
 }else{document.body.style.backgroundImage="url('img/codeImg.jpg')";
 }
